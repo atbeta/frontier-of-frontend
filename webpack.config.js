35edb6e330910182af43710e9df0e27c8ae26f9d
Beta <me@pbeta.me>
2020-09-19 16:34:54 +0800

    build: 分拆及调整webpack配置

--- webpack.config.js
+++ webpack.config.js
@@ -1,32 +1,52 @@
 const path = require('path')
+const webpack = require('webpack')
+const { merge } = require('webpack-merge')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const { CleanWebpackPlugin } = require('clean-webpack-plugin')
 
-module.exports = {
+const commonConfig = {
   entry: './src/main.ts',
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle[hash:8].js'
+    filename: 'bundle-[hash:8].js'
   },
   module: {
     rules: [
       {
-        test: /.tsx?$/,
+        test: /\.tsx?$/,
         use: [
           'babel-loader',
           'ts-loader'
-        ]
+        ],
+        exclude: /node-modules/
       }
     ],
   },
   plugins: [
+    new webpack.ProgressPlugin(),
     new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
       template: './src/index.html',
     })
   ],
+}
+
+const productionConfig = {}
+
+const developmentConfig = {
   devtool: 'source-map',
   devServer: {
     port: 8000
   },
+}
+
+module.exports = (env, options) => {
+  switch(options.mode) {
+    case 'development':
+      return merge(commonConfig, developmentConfig);
+    case 'production':
+      return merge(commonConfig, productionConfig);
+    default:
+      throw new Error('No matching configuration was found!');
+  }
 }
