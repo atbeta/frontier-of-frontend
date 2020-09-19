1eb0989680a1a6adda80beabd11bc36fbb6d6876
Beta <me@pbeta.me>
2020-09-19 17:09:41 +0800

    chore: 增加eslint和prettier配置

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,14 @@
 console.log('Yeah here!')
+
+function sum(x: number, y: number): number {
+  return x + y
+}
+
+const t = sum(100, 200)
+console.log(t)
+
+const appEl = document.querySelector('#app')
+const helloEl = document.createElement('h1')
+helloEl.innerText = 'Hello World!'
+
+appEl && appEl.appendChild(helloEl)
