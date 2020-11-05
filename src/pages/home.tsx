c174fa7507413974d4235ded2e08b70e42db72d7
Beta <me@pbeta.me>
2020-11-05 21:59:06 +0800

    feat: 首页获取分页数据

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,7 +1,36 @@
 import React from 'react'
+import { useRouteMatch } from 'react-router-dom'
+import { useQuery, gql } from '@apollo/client'
+
+const query = gql`
+  query GetPosts($sort: String!, $start: Int!, $limit: Int!) {
+    posts(sort: $sort, start: $start, limit: $limit) {
+      title
+      url
+      id
+    }
+  }
+`
 
 const Home = () => {
-  return <div>Hello World!</div>
+  const { data, loading, error } = useQuery(query, {
+    variables: {
+      sort: 'id:desc',
+      start: 0,
+      limit: 5
+    }
+  })
+  if (loading) {
+    return <div>Loading</div>
+  }
+  const { posts } = data
+  return (
+    <div>
+      {posts.map((post) => (
+        <div key={post.id}>{post.title}</div>
+      ))}
+    </div>
+  )
 }
 
 export default Home
--- src/templates/post.tsx
+++ src/templates/post.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { useRouteMatch } from 'react-router-dom'
 import { useQuery, gql } from '@apollo/client'
+import ReactMarkdown from 'react-markdown'
 
 const query = gql`
   query GetPost($url: String!) {
@@ -16,6 +17,10 @@ export interface SlugRouteParams {
   slug: string
 }
 
+const Image = (props) => {
+  return <img alt="" {...props} style={{ maxWidth: '100%' }} />
+}
+
 const Post = (props) => {
   const match = useRouteMatch<SlugRouteParams>({
     path: '/:slug',
@@ -28,7 +33,21 @@ const Post = (props) => {
       url: slug
     }
   })
-  return <div>hello</div>
+  if (loading) {
+    return <div>Loading</div>
+  }
+  const post = data.posts[0]
+  if (!post) {
+    return <div>404</div>
+  }
+  const { title, content } = post
+  return (
+    <div>
+      <div>{title}</div>
+      <ReactMarkdown source={content} renderers={{ image: Image }} />
+    </div>
+  )
+  // const post = data.posts[0]
 }
 
 export default Post
